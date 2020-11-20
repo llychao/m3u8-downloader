75a4b0b893bd71e6104339ebf72e8760cf896aa0
萌萌站起来 <69511585+llychao@users.noreply.github.com>
2020-11-20 11:58:32 +0800

    Update release-build.sh

--- release-build.sh
+++ release-build.sh
@@ -4,14 +4,14 @@ mkdir "Releases"
 
 # 【darwin/amd64】
 echo "start build darwin/amd64 ..."
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ./Releases/m3u8-downloader-v1.1.0-darwin-amd64
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build  -o ./Releases/m3u8-darwin-amd64 m3u8-downloader.go
 
 # 【linux/amd64】
 echo "start build linux/amd64 ..."
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./Releases/m3u8-downloader-v1.1.0-linux-amd64
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build  -o ./Releases/m3u8-linux-amd64 m3u8-downloader.go
 
 # 【windows/amd64】
 echo "start build windows/amd64 ..."
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ./Releases/m3u8-downloader-v1.1.0-windows-amd64.exe
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build  -o ./Releases/m3u8-windows-amd64.exe m3u8-downloader.go
 
 echo "Congratulations,all build success!"
