40a3602c22672ba6ef778b0d2d998c981548d1b3
萌萌站起来 <69511585+llychao@users.noreply.github.com>
2020-12-03 14:26:58 +0800

    这世界笑了，于是你合群的一起笑了

--- release-build.sh
+++ release-build.sh
@@ -14,4 +14,4 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build  -o ./Releases/m3u8-linux-amd64 m
 echo "start build windows/amd64 ..."
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build  -o ./Releases/m3u8-windows-amd64.exe m3u8-downloader.go
 
-echo "Congratulations,all build success!"
+echo "Congratulations,all build success!!!"
